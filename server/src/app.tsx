cfa59fb5a3412a74aa9ac0d4af16d85ad8a7e740
nasrin haghighi <ns.haghighi@gmail.com>
2023-12-11 15:02:06 +0000

    remove / from clirnt url in the app.tsx

--- server/src/app.tsx
+++ server/src/app.tsx
@@ -19,7 +19,7 @@ const app:Application =express()
 var cors = require('cors');
  app.use(cors(
   {
-  origin:["https://book-store-app-henna.vercel.app/"], 
+  origin:["https://book-store-app-henna.vercel.app"], 
   methods:["POST", "GET"],
   credential:true
 }
